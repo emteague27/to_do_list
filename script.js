01254e9ca47ff8e25e51bae59ff8b8676f99e8bb
Erin <eteague27@gmail.com>
2016-03-07 15:09:49 -0500

    styling

--- script.js
+++ script.js
@@ -8,28 +8,26 @@ $(document).ready(function() {
           	// when I click the link item, this will happen:
             // $(this).addClass('complete');
             // the thing being clicked (this) will have the class complete added to it
-    $(this).remove();
+            $(this).remove();
             // this removes item from list...poof disappear!
-   });
+          });
 
   $("form").submit(function(event){
           	// When the form is submitted this will happen:
-  	 var $itemValue = $('#newTask').val();
+            var $itemValue = $('#newTask').val();
            	// defining variable to call the value of what is in the input form
-  	 event.preventDefault();
+            event.preventDefault();
           	// stop the page from refreshing, which is its default
-  	 $("ul").append('<li>'+'<button></button>' + $itemValue + '</li>');
+            $("ul").append('<li>'+'<button></button>' + $itemValue + '</li>');
   	       // add the variable to the end of the unordered list
-  	 $('li').on('click', function(){
+           $('li').on('click', function(){
   	       // when I click the link item, this will happen:
-      $(this).remove();
+           $(this).remove();
            // the thing being clicked (this) will have the class complete added to it
-    });
+         });
 
-    $('#newTask').val(" ");
-    });
-  	            // took me 20 minutes, but this clears the input box...should've looked below earlier.
+           $('#newTask').val(" ");
+         });
+               
 
- 
-
-});
+              });
